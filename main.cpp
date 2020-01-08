d7eb8d029a6756ad2de9d4b59bb1082594011f3a
Enzo <enzo.allemano@hotmail.com>
2020-01-08 14:39:20 +0100

    Lecture d'un petit fichier
    + affichage du fichier

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 #include "recherche.cpp"
 #include "lecture.cpp"
 
+#include <vector>
+
 using namespace std;
 
 int main() {
+    vector<string> v = lecture("../dico_test.txt");
+
+    for (const auto& i : v) {
+        cout << i << endl;
+    }
 
     return 0;
 }
