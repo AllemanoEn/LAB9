f86d8077689d7bab3ba9a2300cfee02ea00b220e
Nico <n.viotti@hotmail.com>
2020-01-14 14:26:06 +0100

    Ajout commentaire
    Ajout dans lecture.h, lecture.cpp, recherche.h

--- lecture.cpp
+++ lecture.cpp
@@ -1,9 +1,20 @@
+/// \file lecture.cpp
+/// \authors Enzo Allemano, Jonathan Stochetti, Nicolas Viotti
+/// \date 14.01.2020
+///
+/// Remarque :
+///
+/// Compilateur : MinGw-g++ 6.3.0
+
 #include "lecture.h"
 
 #include <fstream>
 
 using namespace std;
-
+///
+/// \brief Permet la lecture d'un fichier ligne par ligne, et les ajoute à la suite dans un vecteur de type string
+/// \param strNomDuFichier : contient le nom du fichier à chercher avec son extension (ex: fichier.txt)
+/// \return Retourne le vecteur contenant les lignes du fichier qui a été lu
 vector<string> lecture(const string& strNomDuFichier){
     vector<string> vDictionnaire;
     vector<string> vError {"Impossible de lire le fichier"};
@@ -30,12 +41,15 @@ vector<string> lecture(const string& strNomDuFichier){
 
     return vDictionnaire;
 }
-
+/// \brief Inverse le sens de lecture du vecteur passé en paramètre ( ex: AB -> BA )
+/// \param vDictionnaire : vecteur string dont le sens va être inversé
 void inverser(vector<string>& vDictionnaire){
     vector<string> vDictionnaireReverse(vDictionnaire.rbegin(),vDictionnaire.rend());
     vDictionnaire = vDictionnaireReverse;
 }
 
+/// \brief Trie le vecteur passé en paramètre de manière alphabétique/croissante avec une comparaison char à char
+/// \param vDictionnaire
 void trier(vector<string>& vDictionnaire){
 
     int iTaille = vDictionnaire.size();
